2279d1d154993e6d9cfb742c21bf89d2b46d99ab
Bach16 <gwyn56717@gmail.com>
2023-09-23 21:38:47 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const modulesRouter = require('./modules.route');
 const coursesRouter = require('./course.route');
+const testsRouter = require('./test.route');
+const lessonsRouter = require('./lesson.route');
 
 const router = Router();
 
 router.use('/modules', modulesRouter);
 router.use('/courses', coursesRouter);
+router.use('/tests', testsRouter);
+router.use('/lessons', lessonsRouter);
 
 module.exports = router;
